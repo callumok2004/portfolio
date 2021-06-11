c6765f19a5652abb527e561270ef8f1ca19c4537
callumok2004 <callumokane123@gmail.com>
2021-06-12 00:16:59 +0100

    wooop

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Navigation from '../components/Navigation';
+import styles from '../styles/pages/Projects.module.scss';
 
 export default function Projects() {
     return (
@@ -11,7 +12,28 @@ export default function Projects() {
             <Navigation user={true} />
 
             <div className="content">
-                Coming soon!
+                <div className={styles.sectionContainer}>
+                    <div className={styles.sectionItem}>
+                        <img src="fyre.png" />
+                        <a target="_blank" href="https://fyredev.xyz/" id="full">Fyre <span>(2018 - Present)</span></a>
+                        <span id="position">Founder & Developer</span>
+                    </div>
+                    <div className={styles.sectionItem}>
+                        <img src="premid.png" />
+                        <a target="_blank" href="https://premid.app" id="full">PreMiD <span>(2019 - 2021)</span></a>
+                        <span id="position">Developer & Support</span>
+                    </div>
+                    <div className={styles.sectionItem}>
+                        <img src="bounce.png" />
+                        <a target="_blank" href="https://itsbounce.net" id="full">Bounce Radio <span>(2019 - Present)</span></a>
+                        <span id="position" style={{marginTop: "8px"}}>Lead Developer & System<br/>Administrator</span>
+                    </div>
+                    <div className={styles.sectionItem}>
+                        <img src="repeat.png" />
+                        <a target="_blank" href="https://repeat.pw" id="full">Repeat Radio <span>(2020 (5 Months))</span></a>
+                        <span id="position">Discord Bot Developer</span>
+                    </div>
+                </div>
             </div>
         </>
     )
